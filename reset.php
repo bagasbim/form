fe943c483d9b8218bb31b3ae66bd484b848a443b
bagasbim <133872707+bagasbim@users.noreply.github.com>
2023-05-17 21:19:46 +0700

    Add files via upload

---
+++ reset.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">s
+    <title>Login</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+</head>
+<body class="bg-light">
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6">
+
+
+        <h4>Reset Password</h4>
+        <p><a href="login.php">kembali ke halaman login</a></p>
+
+        <form action="" method="POST">
+
+            <div class="form-group">
+                <label for="resetEmail">Alamat Email:</label>
+				<input type="email" id="resetEmail" required>
+				<input type="submit" value="Reset Password">
+            </div>
+
+
+        </form>
+            
+        </div>
+
+        <div class="col-md-6">
+            <!-- isi dengan sesuatu di sini -->
+        </div>
+
+    </div>
+</div>
+    
+</body>
+</html>
